dda89a14de395e0c23831f2cc60bf8ffd8f15415
gabriel <ga.lima2205@gmail.com>
2025-03-01 17:00:47 -0300

    init repo

---
+++ terceiro.php
@@ -0,0 +1,13 @@
+<?php 
+	$data = "24 de agosto de 2025"; 
+	$salario = 850.00;
+	$cargo = "estagiário";
+	$idade = 18;
+	$resultado = true;
+
+	if ($resultado) {
+		echo "verdadeiro";
+		echo"<br>";
+		echo "Seu salário é " . $salario ;
+	}
+ ?>
